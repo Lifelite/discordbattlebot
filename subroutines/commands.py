71ef0df5a2b91698065968ddc361dc9aae72ad79
Lifelite <j.t.huntsman@gmail.com>
2023-09-08 17:05:45 -0400

    Testing something crazy

--- subroutines/commands.py
+++ subroutines/commands.py
@@ -1,36 +1,41 @@
 import os
 
 import discord
-
+from discord import ui
 from discord.ext import commands
+from datetime import datetime
 
 from dotenv import load_dotenv, find_dotenv
 
-from data import responses
-from ui import BuildModal
+from subroutines.ui import BuildModal, Dropdown, assignType
+
+MY_GUILD = discord.Object(id=360611536672129025)
 
 
-# intents = discord.Intents.default()
-# intents.message_content = True
-# client = discord.Client(intents=intents)
-# bot = commands.Bot(command_prefix='/', intents=intents)
+class MyClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        # self.tree = app_commands.CommandTree(self)
+
+    # async def setup_hook(self):
+    #     self.tree.copy_global_to(guild=MY_GUILD)
+    #     await self.tree.sync(guild=MY_GUILD)
+
+
+intents = discord.Intents.default()
+client = MyClient(intents=intents)
 
 
 class Bot(commands.Bot):
     def __init__(self):
         intents = discord.Intents.default()
         intents.message_content = True
-
         super().__init__(command_prefix='$', intents=intents)
 
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('------')
 
-
-# @bot.command(name='help')
-# async def _help(ctx):
-#     await ctx.send(responses.response_dict["help"])
 
 bot = Bot()
 
@@ -39,8 +44,8 @@ bot = Bot()
 async def build(ctx):
     # await ctx.send(responses.response_dict["class_build"])
     view = BuildModal()
-    await ctx.send('Build your Character', view=view)
-    await ctx.send(f" {discord.Interaction.user} Submitted a character!")
+
+    await ctx.send("Build your Character!", view=view)
 
 
 @bot.command(name='view')
--- subroutines/ui.py
+++ subroutines/ui.py
@@ -1,8 +1,28 @@
 import discord
-from discord.ui import ChannelSelect
-from data import responses
-import discord.ui as ui
 from functions import Functions
+from data.toon_classes import *
+
+
+# class CharacterInputs(ui.Modal, title=f"Build your Character"):
+#
+#     def __init__(self, c_type):
+#         super().__init__()
+#         self.c_type = c_type
+
+
+def assignType(number):
+    classes = {
+        "Druid": Druid,
+        "Ranger": Ranger,
+        "Mage": Mage,
+        "Paladin": Paladin,
+        "Priest": Priest,
+        "Rogue": Rogue,
+        "Shaman": Shaman,
+        "Warlock": Warlock,
+        "Warrior": Warrior,
+    }
+    return classes[number]()
 
 
 class Dropdown(discord.ui.Select):
@@ -10,61 +30,58 @@ class Dropdown(discord.ui.Select):
     def __init__(self):
         options = [
             discord.SelectOption(label='Druid',
-                                 description="The class of nature...and furry..-er.. I mean fury"),
+                                 description="The class of nature...and furry..-er.. I mean fury", value="Druid"),
 
             discord.SelectOption(label='Ranger',
-                                 description="Bear Gryllz ain't got shit on you"),
+                                 description="Bear Gryllz ain't got shit on you", value="Ranger"),
 
             discord.SelectOption(label='Mage',
-                                 description="lol, Fireball goes brrrrrrrrrr.  Don't touch me."),
+                                 description="lol, Fireball goes brrrrrrrrrr.  Don't touch me.", value="Mage"),
 
             discord.SelectOption(label='Paladin',
-                                 description="Sword and board, with blessing from whatever deity.  Probably a basic bitch."),
+                                 description="Sword and board, with blessing from whatever deity.  Probably a basic bitch.",
+                                 value="Paladin"),
 
             discord.SelectOption(label='Priest',
-                                 description="Healers, gotta love them. Just a bad day away from cult leader."),
+                                 description="Healers, gotta love them. Just a bad day away from cult leader.",
+                                 value="Priest"),
 
             discord.SelectOption(label='Rogue',
-                                 description="Let me guess, you're chaotic neutral."),
+                                 description="Let me guess, you're chaotic neutral.", value="Rogue"),
 
             discord.SelectOption(label='Shaman',
-                                 description="Either overpowered or nerfed to hell...no inbetween."),
+                                 description="Either overpowered or nerfed to hell...no inbetween.", value="Shaman"),
 
             discord.SelectOption(label='Warlock',
-                                 description="See Mages are dumb, just do a deal with the devil and get a similar result, duh."),
+                                 description="See Mages are dumb, just do a deal with the devil and get a similar result, duh.",
+                                 value="Warlock"),
 
             discord.SelectOption(label='Warrior',
-                                 description="Enjoys the simple things in life.  Involves crushing skulls.")
+                                 description="Enjoys the simple things in life.  Involves crushing skulls.",
+                                 value="Warrior")
 
         ]
 
         super().__init__(placeholder='Choose your class...', min_values=1, max_values=1, options=options)
 
+    async def callback(self, response: discord.Interaction):
+        thing = assignType(self.values[0])
+        await response.response.send_modal(thing)
+        self.disabled = True
+        await response.edit_original_response(
+            view=self.view
+        )
 
-# class DropdownView(discord.ui.View):
-# def __init__(self):
-# super().__init__()
 
-# self.add_item(Dropdown())
-# class CharName(discord.ui.TextInput):
-
-
-class BuildModal(discord.ui.Modal, title='Character Builder'):
+class BuildModal(discord.ui.View):
 
     def __init__(self):
-        super().__init__(title="Character Builder")
-        self.select = Dropdown()
-
-        self.c_name = ui.TextInput(label="Character Name:", placeholder="Name your character...", required=True)
-        self.sp_move = ui.TextInput(label="Special Move:", placeholder="Want to add a special move?",required=False)
-        self.add_item(item=self.select)
-        self.add_item(item=self.c_name)
-        self.add_item(item=self.sp_move)
-
-    async def on_submit(self, interaction: discord.Interaction):
-        # await interaction.response.reply(f'Welcome your challenger, {self.c_name} has joined the Tournament!')
-        t_class = Dropdown.values
-        user = discord.Interaction.user
-
-        f = Functions(self.c_name, self.sp_move, t_class)
-        f.toon_upload(user)
+        super().__init__()
+        self.add_item(Dropdown())
+    # async def on_submit(self, interaction: discord.Interaction):
+    #     # await interaction.response.reply(f'Welcome your challenger, {self.c_name} has joined the Tournament!')
+    #     t_class = Dropdown.values
+    #     user = discord.Interaction.user
+    #
+    #     f = Functions(self.c_name, self.sp_move, t_class)
+    #     f.toon_upload(user)
